59e8b9b9e97521762198b3d5207840d95f33f63e
Sun Woo Kim <53247724+sunwoo101@users.noreply.github.com>
2025-05-04 05:42:06 +1000

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,8 @@
+import backgroundImage from './assets/background.png';
+
 function Home() {
     return (
-        <section id="home" className="w-full h-screen bg-[url('./src/assets/background.png')] bg-cover bg-center bg-no-repeat flex items-center justify-center">
+        <section style={{ backgroundImage: `url(${backgroundImage})` }} id="home" className="w-full h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center">
             <div className="w-full h-full flex flex-col justify-center items-center text-center mx-auto px-4 py-12">
                 <div className="w-full flex-1 flex flex-col justify-center items-center gap-4">
                     <div>
