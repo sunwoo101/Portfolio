f1712fd8f89b7e1f554a48fab7253dee82579d12
Sun Woo Kim <53247724+sunwoo101@users.noreply.github.com>
2025-05-10 16:09:34 +1000

    Fixed responsiveness

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,15 +3,15 @@ import backgroundImage from './assets/background.png';
 function Home() {
     return (
         <section id="home" style={{ backgroundImage: `url(${backgroundImage})` }} className="relative w-full h-screen bg-cover bg-center bg-no-repeat">
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center px-4 py-12 flex flex-col items-center gap-4">
-                <p className="text-palette-text text-xl sm:text-3xl leading-none">
+            <div className="absolute w-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center px-4 py-12 flex flex-col items-center gap-4">
+                <p className="w-full text-palette-text text-xl sm:text-3xl leading-none">
                     <i className="fas fa-thumbtack text-palette-text text-xl"></i> Sydney, Australia
                 </p>
-                <h1 className="text-palette-text text-4xl sm:text-5xl leading-none">Sun Woo Kim</h1>
-                <h2 className="text-palette-text text-2xl sm:text-3xl leading-none">Full-Stack Software Engineer</h2>
+                <h1 className="w-full text-palette-text text-4xl sm:text-5xl leading-none">Sun Woo Kim</h1>
+                <h2 className="w-full text-palette-text text-2xl sm:text-3xl leading-none">Full-Stack Software Engineer</h2>
             </div>
 
-            <div className="absolute top-[calc(50%+8rem)] left-1/2 transform -translate-x-1/2">
+            <div className="absolute w-full top-[calc(50%+8rem)] left-1/2 transform -translate-x-1/2">
                 <a href="#about" className="flex flex-col items-center">
                     <h3 className="text-palette-text text-2xl mb-1">Scroll</h3>
                     <i className="fa-solid fa-angles-down text-white text-3xl"></i>
